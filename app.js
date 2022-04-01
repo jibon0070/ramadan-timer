4f98b873d71701c54b4e88589eeb1449db4a8c0c
Jibon <atikurrahaman386@gmail.com>
2022-04-02 05:51:54 +0600

    03. updated with github co pilot

--- app.js
+++ app.js
@@ -30,85 +30,79 @@ const data = {
     "2022-05-01": ['03:59 AM', '06:31 PM'],
     "2022-05-02": ['03:58 AM', '06:31 PM'],
 }
-const separator = ":"
-console.time('Load Time')
-/**
- * 
- * @param {Number} number 
- * @returns {String}
- */
-const number_padding = (number) => {
-    if (number < 10) return '0' + number;
-    return '' + number;
+
+const separator = ':';
+
+function number_padding(num) {
+    return num < 10 ? '0' + num : num;
 }
-/**
- * 
- * @param {Date} date 
- * @returns {String}
- */
-const date_format = (date) => {
-    return `${number_padding(date.getFullYear())}-${number_padding(date.getMonth() + 1)}-${number_padding(date.getDate())}`
+
+function date_format(date) {
+    return date.getFullYear() + '-' + number_padding(date.getMonth() + 1) + '-' + number_padding(date.getDate());
 }
-/**
- * 
- * @param {Date} time 
- * @returns {String}
- */
-const time_format = (time, show_second = false) => {
-    const hour = number_padding(time.getHours() % 12)
-    return `${hour == '00' ? '12' : hour}:${number_padding(time.getMinutes())}${!show_second ? '' : ':'+number_padding(time.getSeconds())} ${time.getHours() < 12 ? "AM" : "PM"}`
+
+function time_format(time, show_second = false) {
+    let hour = number_padding(time.getHours() % 12);
+    let minute = number_padding(time.getMinutes());
+    let second = number_padding(time.getSeconds());
+    return `${hour == '00' ? '12' : hour}${separator}${minute}${show_second ? separator + second : ''} ${time.getHours() >= 12 ? 'PM' : 'AM'}`;
 }
-addEventListener("DOMContentLoaded", () => {
-    const main = () => {
-        let current_date = new Date(date_format(new Date()));
+
+addEventListener('DOMContentLoaded', () => {
+    function main() {
+        let current_date = new Date();
         const start_date = new Date(Object.keys(data)[0]);
-        const end_date = new Date(Object.keys(data)[Object.keys(data).length - 1])
+        const end_date = new Date(Object.keys(data)[Object.keys(data).length - 1]);
         if (current_date < start_date) {
-            current_date = date_format(start_date)
+            current_date = date_format(start_date);
+        } else if (current_date > end_date) {
+            window.close();
+        } else {
+            current_date = date_format(current_date);
         }
-        else if (current_date > end_date) window.close();
-        else current_date = date_format(current_date)
-        let [seheri, iftar] = data[current_date];
-        seheri = new Date(current_date + ' ' + seheri).getTime()
-        iftar = new Date(current_date + ' ' + iftar).getTime()
-        const time = new Date().getTime()
-        const status_element = document.querySelector('#status')
-        let remaining
-        if (time < seheri) {
-            remaining = Math.round((seheri - time) / 1000);
-            status_element.innerHTML = `remaining for sehri (${time_format(new Date(seheri))})`;
+        let [sehri, iftar] = data[current_date];
+        sehri = new Date(current_date + ' ' + sehri).getTime();
+        iftar = new Date(current_date + ' ' + iftar).getTime();
+        const time = new Date().getTime();
+        const status_element = document.querySelector('#status');
+        let remaining;
+        if (time < sehri) {
+            remaining = sehri - time;
+            status_element.innerText = `Sehri Time ${time_format(new Date(sehri))}`;
+        } else {
+            remaining = iftar - time;
+            status_element.innerText = `Iftar Time ${time_format(new Date(iftar))}`;
         }
-        else {
-            remaining = Math.round((iftar - time) / 1000)
-            status_element.innerHTML = `remaining for iftar (${time_format(new Date(iftar))})`;
-        }
-        const second = number_padding(remaining % 60)
-        const minute = number_padding(Math.floor((remaining / 60) % 60))
-        const hour = number_padding(Math.floor((remaining / 60 / 60) % 24))
-        const day = number_padding(Math.floor(remaining / 60 / 60 / 24))
+        const seconds = Math.floor(remaining / 1000) % 60;
+        const minutes = Math.floor(remaining / (1000 * 60)) % 60;
+        const hours = Math.floor(remaining / (1000 * 60 * 60)) % 24;
+        const days = Math.floor(remaining / (1000 * 60 * 60 * 24));
         const day_element = document.querySelector('#day');
-        if (day == '00')
-            day_element.innerText = ''
-        else
-            day_element.innerText = day + separator
-        const hour_element = document.querySelector('#hour')
-        if (day == '00' && hour == '00')
-            hour_element.innerHTML = ''
-        else
-            hour_element.innerHTML = hour + separator
-        const minute_element = document.querySelector('#minute')
-        if (day == '00' && hour == '00' && minute == '00')
-            minute_element.innerHTML = ''
-        else
-            minute_element.innerHTML = minute + separator
-        const second_element = document.querySelector('#second')
-        second_element.innerHTML = second
-        const time_element = document.querySelector('#time')
-        time_element.innerHTML = time_format(new Date(), true)
+        const hour_element = document.querySelector('#hour');
+        const minute_element = document.querySelector('#minute');
+        const second_element = document.querySelector('#second');
+        if (days == 0) {
+            day_element.innerText = '';
+        } else {
+            day_element.innerText = number_padding(days) + separator;
+        }
+        if(days == 0 && hours == 0) {
+            hour_element.innerText = '';
+        } else {
+            hour_element.innerText = number_padding(hours) + separator;
+        }
+        if(days == 0 && hours == 0 && minutes == 0) {
+            minute_element.innerText = '';
+        } else {
+            minute_element.innerText = number_padding(minutes) + separator;
+        }
+        if(days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+            second_element.innerText = '';
+        } else {
+            second_element.innerText = number_padding(seconds);
+        }
+        document.querySelector('#time').innerText = time_format(new Date(time), true);
     }
-    main()
-    setInterval(() => {
-        main()
-    }, 1000);
-    console.timeEnd('Load Time')
+    main();
+    setInterval(main, 1000);
 })
