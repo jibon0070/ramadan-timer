673e4ea1430b9ef4a853df164b8ba34bf04ad5f9
Jibon <atikurrahaman386@gmail.com>
2022-04-03 23:17:52 +0600

    07. bug fix

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const data = {
     "2022-04-03": ['04:27 AM', '06:19 PM'],
-    "2022-04-04": ['04:26 AM', '06:19 PM'],
-    "2022-04-05": ['04:24 AM', '06:20 PM'],
+    "2022-04-04": ['04:27 AM', '06:19 PM'],
+    "2022-04-05": ['04:26 AM', '06:19 PM'],
     "2022-04-06": ['04:24 AM', '06:20 PM'],
-    "2022-04-07": ['04:23 AM', '06:21 PM'],
-    "2022-04-08": ['04:22 AM', '06:21 PM'],
-    "2022-04-09": ['04:21 AM', '06:21 PM'],
-    "2022-04-10": ['04:20 AM', '06:22 PM'],
-    "2022-04-11": ['04:19 AM', '06:22 PM'],
-    "2022-04-12": ['04:18 AM', '06:23 PM'],
-    "2022-04-13": ['04:17 AM', '06:23 PM'],
-    "2022-04-14": ['04:15 AM', '06:23 PM'],
-    "2022-04-15": ['04:14 AM', '06:24 PM'],
-    "2022-04-16": ['04:13 AM', '06:24 PM'],
-    "2022-04-17": ['04:12 AM', '06:24 PM'],
-    "2022-04-18": ['04:11 AM', '06:25 PM'],
-    "2022-04-19": ['04:10 AM', '06:25 PM'],
-    "2022-04-20": ['04:09 AM', '06:26 PM'],
-    "2022-04-21": ['04:08 AM', '06:26 PM'],
-    "2022-04-22": ['04:07 AM', '06:27 PM'],
-    "2022-04-23": ['04:06 AM', '06:27 PM'],
-    "2022-04-24": ['04:05 AM', '06:28 PM'],
+    "2022-04-07": ['04:24 AM', '06:20 PM'],
+    "2022-04-08": ['04:23 AM', '06:21 PM'],
+    "2022-04-09": ['04:22 AM', '06:21 PM'],
+    "2022-04-10": ['04:21 AM', '06:21 PM'],
+    "2022-04-11": ['04:20 AM', '06:22 PM'],
+    "2022-04-12": ['04:19 AM', '06:22 PM'],
+    "2022-04-13": ['04:18 AM', '06:23 PM'],
+    "2022-04-14": ['04:17 AM', '06:23 PM'],
+    "2022-04-15": ['04:15 AM', '06:23 PM'],
+    "2022-04-16": ['04:14 AM', '06:24 PM'],
+    "2022-04-17": ['04:13 AM', '06:24 PM'],
+    "2022-04-18": ['04:12 AM', '06:24 PM'],
+    "2022-04-19": ['04:11 AM', '06:25 PM'],
+    "2022-04-20": ['04:10 AM', '06:25 PM'],
+    "2022-04-21": ['04:09 AM', '06:26 PM'],
+    "2022-04-22": ['04:08 AM', '06:26 PM'],
+    "2022-04-23": ['04:07 AM', '06:27 PM'],
+    "2022-04-24": ['04:06 AM', '06:27 PM'],
     "2022-04-25": ['04:05 AM', '06:28 PM'],
-    "2022-04-26": ['04:04 AM', '06:29 PM'],
-    "2022-04-27": ['04:03 AM', '06:29 PM'],
-    "2022-04-28": ['04:02 AM', '06:29 PM'],
-    "2022-04-29": ['04:01 AM', '06:30 PM'],
-    "2022-04-30": ['04:00 AM', '06:30 PM'],
-    "2022-05-01": ['03:59 AM', '06:31 PM'],
-    "2022-05-02": ['03:58 AM', '06:31 PM'],
+    "2022-04-26": ['04:05 AM', '06:28 PM'],
+    "2022-04-27": ['04:04 AM', '06:29 PM'],
+    "2022-04-28": ['04:03 AM', '06:29 PM'],
+    "2022-04-29": ['04:02 AM', '06:29 PM'],
+    "2022-04-30": ['04:01 AM', '06:30 PM'],
+    "2022-04-01": ['04:00 AM', '06:30 PM'],
+    "2022-05-02": ['03:59 AM', '06:31 PM'],
+    "2022-05-03": ['03:58 AM', '06:31 PM'],
 }
 
 const separator = ':';
@@ -50,6 +51,7 @@ function time_format(time, show_second = false) {
 
 addEventListener('DOMContentLoaded', () => {
     function main() {
+        // debugger
         let current_date = new Date();
         const start_date = new Date(Object.keys(data)[0]);
         const end_date = new Date(Object.keys(data)[Object.keys(data).length - 1]);
@@ -63,7 +65,14 @@ addEventListener('DOMContentLoaded', () => {
         let [sehri, iftar] = data[current_date];
         sehri = new Date(current_date + ' ' + sehri).getTime();
         iftar = new Date(current_date + ' ' + iftar).getTime();
-        const time = new Date().getTime();
+        let time = new Date().getTime();
+        if (time > iftar) {
+            current_date = date_format(new Date(new Date(current_date).getTime() + 1000 * 60 * 60 * 24));
+            [sehri, iftar] = data[current_date];
+            sehri = new Date(current_date + ' ' + sehri).getTime();
+            iftar = new Date(current_date + ' ' + iftar).getTime();
+            time = new Date().getTime();
+        }
         const status_element = document.querySelector('#status');
         let remaining;
         if (time < sehri) {
@@ -136,7 +145,8 @@ addEventListener('dblclick', () => {
 })
 
 addEventListener('keydown', (e) => {
-    if(e.key === 'f') {
+    //keycode of f or keycode of ফ
+    if (e.keyCode === 70 || e.keyCode === 231) {
         //fullscreen
         if (document.fullscreenElement) {
             document.exitFullscreen();
