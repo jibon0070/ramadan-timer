65c5e47cefc7743e1e27d58d7f82bf4b5d31cdaa
A.R. Jibon <atikurrahaman386@gmail.com>
2025-02-02 13:42:57 +0600

    starting from scratch

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-// noinspection JSUnusedGlobalSymbols
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
